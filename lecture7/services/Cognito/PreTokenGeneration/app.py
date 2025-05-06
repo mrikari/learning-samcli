832660661aa2598f2a800b2e99bb24fc9c5db2e7
k.ikari <61904007+mrikari@users.noreply.github.com>
2025-05-06 23:18:10 +0900

    TokenGeneration時の不要な処理を削除

--- lecture7/services/Cognito/PreTokenGeneration/app.py
+++ lecture7/services/Cognito/PreTokenGeneration/app.py
@@ -1,8 +1,4 @@
 import os
-import boto3
-
-dynamodb = boto3.resource("dynamodb")
-table = dynamodb.Table(os.environ["ROLE_TABLE_NAME"])
 
 def lambda_handler(event, context):
     user_attrs = event["request"]["userAttributes"]
@@ -12,29 +8,11 @@ def lambda_handler(event, context):
     if not role_id:
         raise Exception("Missing custom:role")
 
-    try:
-        response = table.get_item(Key={"role_id": role_id})
-        role = response.get("Item", {})
-    except Exception as e:
-        raise Exception(f"Failed to fetch role info: {str(e)}")
-
-    is_super_user = role.get("is_super_user", False)
-    allowed_operations = role.get("allowed_operations", [])
-
     claims = {
         "username": username,
         "role": role_id,
-        "is_super_user": is_super_user
     }
 
-    if not is_super_user and allowed_operations:
-        # allowed_operations: List[str] → CSV形式で格納
-        claims["allowed_operations"] = ",".join(allowed_operations)
-
-    event["response"] = {
-        "claimsOverrideDetails": {
-            "claimsToAddOrOverride": claims
-        }
-    }
+    event["response"] = {"claimsOverrideDetails": {"claimsToAddOrOverride": claims}}
 
     return event
