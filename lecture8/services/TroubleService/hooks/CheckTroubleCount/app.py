66fd244b70b0a7456283e5b868b0e20db819fd04
k.ikari <61904007+mrikari@users.noreply.github.com>
2025-05-21 01:40:36 +0900

    fix

---
+++ lecture8/services/TroubleService/hooks/CheckTroubleCount/app.py
@@ -0,0 +1,48 @@
+from datetime import datetime, timedelta
+import json
+import os
+import time
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('TroubleTable')
+LOG_GROUP_NAME = os.environ['LOG_GROUP_NAME']
+cloudwatchlogs = boto3.client('logs')
+
+def lambda_handler(event, context):
+
+    # ログストリーム名は、CheckTroubleCount-YYYY-MM-DD-HH の形式である
+    # １時間前のものがあるかを確認する
+    now = datetime.now()
+    one_hour_ago = now - timedelta(hours=1)
+    one_hour_ago_str = one_hour_ago.strftime('%Y-%m-%d-%H')
+    
+    # ログストリームが存在しない場合は処理を終了
+    log_streams = cloudwatchlogs.describe_log_streams(
+        logGroupName=LOG_GROUP_NAME,
+        logStreamNamePrefix=f"CheckTroubleCount-{one_hour_ago_str}",
+    )
+
+    if len(log_streams['logStreams']) == 0:
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "count": 0,
+            }),
+        }
+
+    # あれば、そのログストリームのログイベントを取得し、行数をカウントする
+    count = 0
+    for log_stream in log_streams['logStreams']:
+        log_events = cloudwatchlogs.get_log_events(
+            logGroupName=LOG_GROUP_NAME,
+            logStreamName=log_stream['logStreamName'],
+        )
+        count += len(log_events['events'])
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "count": count,
+        }),
+    }
--- lecture8/services/TroubleService/hooks/CreateTrouble/app.py
+++ lecture8/services/TroubleService/hooks/CreateTrouble/app.py
@@ -1,11 +1,52 @@
 import json
+import os
+import time
 import boto3
 
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('TroubleTable')
+LOG_GROUP_NAME = os.environ['LOG_GROUP_NAME']
+cloudwatchlogs = boto3.client('logs')
+
 def lambda_handler(event, context):
+
+    # ログストリームが存在しない場合は作成する
+    log_streams = cloudwatchlogs.describe_log_streams(
+        logGroupName=LOG_GROUP_NAME
+    )
+    log_stream_name = f'CheckTroubleCount-{time.strftime("%Y-%m-%d-%H")}'
+    if len(log_streams['logStreams']) == 0:
+        cloudwatchlogs.create_log_stream(
+            logGroupName=LOG_GROUP_NAME,
+            logStreamName=log_stream_name
+        )
+
+    # DynamoDBStreamがInsertされたら、rowの内容を取得して、categoryが "緊急" であるかどうかを判定する
+    # 緊急であれば CloudWatchLogsにログを出力する
+    # 緊急でなければ何もしない
     print(event)
+    for record in event['Records']:
+        if record['eventName'] == 'INSERT':
+            row = record['dynamodb']['NewImage']
+            category = row['category']['S']
+            message = row['message']['S']
+            if category == '緊急':
+                # ログストリームは1時間ごとに切り替わるよう命名する
+                cloudwatchlogs.put_log_events(
+                    logGroupName=LOG_GROUP_NAME,
+                    logStreamName=log_stream_name,
+                    logEvents=[
+                        {
+                            'timestamp': int(time.time() * 1000),
+                            # unicodeにならないようにする
+                            'message': json.dumps(message, ensure_ascii=False)
+                        }
+                    ]
+                )
+
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": "OK",
         }),
     }
